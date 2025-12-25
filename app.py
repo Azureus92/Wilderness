422dc007f848634a0bc82c718a683f2387fe264a
Peter <a1831319@adelaide.edu.au>
2025-12-25 17:49:55 +1030

    modified hex extractor, added route

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
 from flask import render_template
+from werkzeug.utils import secure_filename
+import sqlite3
+from hex_mangler import mangle
+import os
+
+UPLOAD_FOLDER = './static/images/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def login():
@@ -11,31 +22,50 @@ def login():
 def map():
     return render_template('index.html')
 
-import sqlite3
-conn = sqlite3.connect("test.db")
+@app.route('/upload-map', methods=['GET', 'POST'])
+def upload_map():
+    if request.method == 'POST':
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                file.save(app.config['UPLOAD_FOLDER'] + '/' + filename)
+                with sqlite3.connect("test.db") as conn:
+                    cur = conn.cursor()
+                    mangle(app.config['UPLOAD_FOLDER'] + '/' + filename, 20, 25, conn, cur)
+                    res = cur.execute("SELECT * FROM hexes").fetchall()
+                    print(len(res))
+                return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+    <input type=file name=file>
+    <input type=submit value=Upload>
+    </form>
+    '''
 
-cur = conn.cursor()
+from flask import send_from_directory
 
-res = cur.execute("SELECT name FROM sqlite_master")
-sch = res.fetchall()
-if (sch is None or ('users',) not in sch):
-    cur.execute("CREATE TABLE users(username, password, hexes)")
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
-if (sch is None or ('hexes',) not in sch):
-    cur.execute("CREATE TABLE hexes(id, img, description)")
 
-res = cur.execute("SELECT name FROM sqlite_master")
-
-# import base64
-# with open("./static/images/hexes/0_0.png", 'rb') as file:
-#     blobdata = file.read()
-
-# cur.execute("INSERT INTO hexes (id, img, description) VALUES (?, ?, ?)", ('0_0', blobdata, "A hex in Tasmania"))
-# conn.commit()
-
-res = cur.execute("SELECT * FROM hexes").fetchone()
+with sqlite3.connect("test.db") as conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users(username, password, hexes)")
+    cur.execute("CREATE TABLE IF NOT EXISTS hexes(id, x, y, description)")
 
 from PIL import Image
 import io
 
 # Image.open(io.BytesIO(res[1]))
+
---
+++ hex_mangler.py
@@ -0,0 +1,83 @@
+from PIL import Image, ImageOps
+import math
+import os
+import glob
+import random
+
+def mangle(i, hc_width, hc_height, conn, cur):
+    cur.execute("DELETE FROM hexes")
+    conn.commit()
+    files = glob.glob('./static/images/hexes/*')
+    for f in files:
+        os.remove(f)
+
+    res = 20
+
+    wmap = Image.open(i).convert('RGBA')
+
+    map_pixels = wmap.load()
+
+    ids = random.sample(range(hc_width * hc_height), hc_width * hc_height)
+
+    w = round(wmap.width / hc_width)
+    h = round(wmap.height / (hc_height + 0.5))
+
+    cursor_x = round(w / 2)
+    cursor_y = round(h / 2)
+
+    from queue import Queue
+    to_visit = Queue()
+    dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+    i = 0
+    for ptx in range(hc_width):
+        for pty in range(hc_height):
+            mask = Image.new('RGBA', wmap.size, (255, 255, 255, 255))
+            mask_pixels = mask.load()
+            to_visit.put((cursor_x, cursor_y))
+
+            visited = []
+            lm = 999999
+            rm = 0
+            um = 999999
+            dm = 0
+
+            while to_visit.empty() != True:
+                pt = to_visit.get()
+                if pt in visited:
+                    continue
+                visited.append(pt)
+                x = pt[0]
+                y = pt[1]
+                lm = min(x, lm)
+                rm = max(x, rm)
+                um = min(y, um)
+                dm = max(y, dm)
+                mask_pixels[x, y] = (0,0,0,255)
+                for d in dirs:
+                    nx = d[0] + x 
+                    ny = d[1] + y
+                    if ((nx > 0 and nx < wmap.width and ny > 0 and ny < wmap.height) and
+                        (map_pixels[nx, ny][0] > res or map_pixels[nx, ny][1] > res or map_pixels[nx, ny][2] > res) and
+                        mask_pixels[nx, ny] != (0,0,0,255)):
+                        to_visit.put((nx, ny))
+
+            mask = mask.point(lambda x: x < 100 and 255).convert('L')
+
+            tmap = wmap
+
+            tmap.putalpha(mask)
+
+            tmap = tmap.crop((lm, um, rm, dm)) 
+            cur.execute("INSERT INTO hexes (id, x, y, description) VALUES (?, ?, ?, ?)", (ids[i], ptx, pty, "A hex in Tasmania"))
+            conn.commit()
+
+            tmap.save('static/images/hexes/' + str(ids[i]) + '.png')
+            i+=1
+            cursor_y = (cursor_y + h) % wmap.height
+
+        cursor_x = (cursor_x + w) % wmap.width
+        if (ptx % 2 == 0):
+            cursor_y = h
+        else:
+            cursor_y = round(h / 2)
